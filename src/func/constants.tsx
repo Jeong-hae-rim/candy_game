e78b14b79d52ae1dbc36bfe73cc886abcce83c56
jeong-hae-rim <usemono33@gmail.com>
2025-11-20 22:44:21 +0900

    폰트 수정 및 패스워드 추가

--- src/func/constants.tsx
+++ src/func/constants.tsx
@@ -1,3 +1,6 @@
 export const SESSION_KEY = "pw_unlocked_v1";
 export const PASSWORD = "35716824";
 export const PASSWORD2 = "지금은좋아해";
+export const PASSWORD3 = "지금은 좋아해";
+export const PASSWORD4 = "지금은 좋아해!";
+export const PASSWORD5 = "지금은좋아해!";
--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,14 @@
 import "./App.css";
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import { SESSION_KEY, PASSWORD, PASSWORD2 } from "../func/constants";
+import {
+  SESSION_KEY,
+  PASSWORD,
+  PASSWORD2,
+  PASSWORD3,
+  PASSWORD4,
+  PASSWORD5,
+} from "../func/constants";
 
 export default function App() {
   const navigate = useNavigate();
@@ -21,7 +28,9 @@ export default function App() {
       setMessage("비밀번호를 입력하세요.");
       return;
     }
-    if (input === PASSWORD || input === PASSWORD2) {
+    if (
+      [PASSWORD, PASSWORD2, PASSWORD3, PASSWORD4, PASSWORD5].includes(input)
+    ) {
       // ✅ 현재 탭 세션에서는 항상 통과되도록 세션키 저장
       sessionStorage.setItem(SESSION_KEY, "1");
       // ✅ 체크한 경우에만 '지속 저장'
@@ -38,6 +47,7 @@ export default function App() {
     <div className="container">
       <div className="card">
         <h1>비밀결사대클럽432</h1>
+        <p>secretclub432</p>
 
         <div className="input-wrap">
           <input
