7498ef43877f240a7b32b69ec110d0921bfba423
jeong-hae-rim <usemono33@gmail.com>
2026-05-21 02:53:27 +0900

    feat: 수정

--- src/routes.tsx
+++ src/routes.tsx
@@ -1,10 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import App from "./pages/App";
-import Secretpage from "./pages/SecretPage";
 import NotFound from "./pages/404";
 import GamePage from "./pages/GamePage";
-import RollingPage from "./pages/RollingPage";
-import MdPage from "./pages/MdPage";
 
 const Router = () => {
   return (
